96ed4e638f873de73ff131fc21b3fbdd95100381
dsl2022 <2015rpro@gmail.com>
2023-08-14 21:25:09 -0400

    updated cors

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const pokemonData = require("./database.json")
 const PORT = 5000;
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/',(req, res)=>{    
     res.send("Hello")
 });
 
-app.get('/all-pokemons',(req, res)=>{
-    console.    
+app.get('/all-pokemons',(req, res)=>{    
     res.send(JSON.stringify(pokemonData))
 });
 
