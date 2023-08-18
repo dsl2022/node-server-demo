299c4a1bea84b10ea0eff5fea083e924e6bda5ff
dsl2022 <2015rpro@gmail.com>
2023-08-17 22:08:39 -0400

    updated

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const pokemonData = require("./database.json")
+const strangerData = require("./strangerDB.json")
 const PORT = 5000;
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
 
-app.get('/',(req, res)=>{    
+app.get('/api',(req, res)=>{    
     res.send("Hello")
 });
 
-app.get('/all-pokemons',(req, res)=>{    
+app.get('/api/all-pokemons',(req, res)=>{   
+    // query database
+    // process return data
+    // format the data
+    // return the data 
     res.send(JSON.stringify(pokemonData))
 });
-
+app.get("/posts",(req,res)=>{
+    console.log(strangerData)
+    res.send(JSON.stringify(strangerData))
+})
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT} successfully`)
 })
